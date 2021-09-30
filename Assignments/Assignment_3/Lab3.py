0fb11118fbc9fc5a491d5db573542260da4e90f0
Levi Estabrook <mrlevil2021@gmail.com>
2021-09-29 20:13:30 -0500

    Lab3 part 3

--- Assignments/Assignment_3/Lab3.py
+++ Assignments/Assignment_3/Lab3.py
@@ -15,141 +15,73 @@ Y or N are the only valid responses to this.
 
 print('Welcome to the Flarsheim Guesser!\n')
 
-T_rem0 = []
-T_rem1 = []
-T_rem2 = []
-for t in range(1,101):
-    if t % 3 == 0:
-        T_rem0.append(t)
-    elif t % 3 == 1:
-        T_rem1.append(t)
-    elif t % 3 == 2:
-        T_rem2.append(t)
-
-F_rem0 = []
-F_rem1 = []
-F_rem2 = []
-F_rem3 = []
-F_rem4 = []
-for f in range(1,101):
-    if f % 5 == 0:
-        F_rem0.append(f)
-    elif f % 5 == 1:
-        F_rem1.append(f)
-    elif f % 5 == 2:
-        F_rem2.append(f)
-    elif f % 5 == 3:
-        F_rem3.append(f)
-    elif f % 5 == 4:
-        F_rem4.append(f)
-
-S_rem0 = []
-S_rem1 = []
-S_rem2 = []
-S_rem3 = []
-S_rem4 = []
-S_rem5 = []
-S_rem6 = []
-for s in range(1,101):
-    if s % 7 == 0:
-        S_rem0.append(s)
-    elif s % 7 == 1:
-        S_rem1.append(s)
-    elif s % 7 == 2:
-        S_rem2.append(s)
-    elif s % 7 == 3:
-        S_rem3.append(s)
-    elif s % 7 == 4:
-        S_rem4.append(s)
-    elif s % 7 == 5:
-        S_rem5.append(s)
-    elif s % 7 == 6:
-        S_rem6.append(s)
-
-
-
-def Flarsheim(rem3,rem5,rem7):
-    if rem3 == 0:
-        Flarsheimlist = T_rem0
-    elif rem3 == 1:
-        Flarsheimlist = T_rem1
-    elif rem3 == 2:
-        Flarsheimlist = T_rem2
-    if rem5 == 0:
-        Flarsheimlist.append(F_rem0)
-    elif rem5 == 1:
-        Flarsheimlist.append(F_rem1)
-    elif rem5 == 2:
-        Flarsheimlist.append(F_rem2)
-    elif rem5 == 3:
-        Flarsheimlist.append(F_rem3)
-    elif rem5 == 4:
-        Flarsheimlist.append(F_rem4)
-    if rem7 == 0:
-        Flarsheimlist.append(S_rem0)
-    elif rem7 == 1:
-        Flarsheimlist.append(S_rem1)
-    elif rem7 == 2:
-        Flarsheimlist.append(S_rem2)
-    elif rem7 == 3:
-        Flarsheimlist.append(S_rem3)
-    elif rem7 == 4:
-        Flarsheimlist.append(S_rem4)
-    elif rem7 == 5:
-        Flarsheimlist.append(S_rem5)
-    elif rem7 == 6:
-        Flarsheimlist.append(S_rem6)
-    print(Flarsheimlist)
-'''
-print(T_rem0)
-print(T_rem1)
-print(T_rem2)
-print(F_rem0)
-print(F_rem1)
-print(F_rem2)
-print(F_rem3)
-print(F_rem4)
-print(S_rem0)
-print(S_rem1)
-print(S_rem2)
-print(S_rem3)
-print(S_rem4)
-print(S_rem5)
-print(S_rem6)
-'''
-
-
 playing = True
 while playing == True:
     print('Please think of a number between and including 1 and 100.\n')
 
     valid3 = None
     while valid3 != True:
-        div3 = int(input('What is the remainder when your number is divided by 3 ?'))
+        div3 = int(input('What is the remainder when your number is divided by 3? '))
         if div3 < 0:
             print('The value entered must be 0 or greater')
         elif div3 > 2:
             print('The value entered must be less than 3')
         else:
+            print()
             valid3 = True
     
     valid5 = None
     while valid5 != True:
-        div5 = int(input('What is the remainder when your number is divided by 5 ?'))
+        div5 = int(input('What is the remainder when your number is divided by 5? '))
         if div5 < 0:
             print('The value entered must be 0 or greater')
         elif div5 > 4:
             print('The value entered must be less than 5')
         else:
+            print()
             valid5 = True
     
     valid7 = None
     while valid7 != True:
-        div7 = int(input('What is the remainder when your number is divided by 7 ?'))
+        div7 = int(input('What is the remainder when your number is divided by 7? '))
         if div7 < 0:
             print('The value entered must be 0 or greater')
         elif div7 > 6:
             print('The value entered must be less than 7')
         else:
+            print()
             valid7 = True
-    Flarsheim(div3,div5,div7)
+
+    list1 = []
+    for x in range(0,101):
+        if x % 3 == div3:
+            list1.append(x)
+
+    list2 = []
+    for y in list1:
+        if y % 5 == div5:
+            list2.append(y)
+
+    for z in list2:
+        if z % 7 == div7:
+            user_number = z
+            break
+        else:
+            user_number = None
+
+    if user_number != None:
+        print('Your number was', user_number)
+        print('How amazing is that?\n')
+    else:
+        print('Your number wasn\'t between 1 and 100.\n')
+    
+    validplay = None
+    while validplay == None:
+        play_again = input('Do you want to play again? Y to continue, N to quit  ==> ')
+        if play_again == ('n' or 'N'):
+            validplay = True
+            playing = False
+        elif play_again == ('y' or 'Y'):
+            validplay = True
+            print()
+    
