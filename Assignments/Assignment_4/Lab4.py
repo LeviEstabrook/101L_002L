aed8dcce6e7731bbe2d497fcb1359f9159ea5b1f
Levi Estabrook <mrlevil2021@gmail.com>
2021-10-04 13:52:28 -0500

    Lab4 part 2

--- Assignments/Assignment_4/Lab4.py
+++ Assignments/Assignment_4/Lab4.py
@@ -17,30 +17,30 @@ If the user matches 2 numbers, they will win 3 times their wager.
 import math
 import random
 
-
+# take 1
 def play_again():
     ''' Asks the user if they want to play again, returns False if N or NO, and True if Y or YES. Keeps asking until they respond yes '''
-    valid = None
-    while valid == None:
+    while True:
         play = input('Do you want to play again? ==> ').lower()
         if play == 'y' or 'yes':
             return True
-            valid = True
         elif play == 'n' or 'no':
             return False
-            valid = True
         else:
             print('\nYou must enter Y/YES/N/NO to continue.  Please try again')
 
-
-
-    return True
-     
-def get_wager(bank : int) -> int:
+# take 1
+def get_wager(bank):
     ''' Asks the user for a wager chip amount.  Continues to ask if they result is <= 0 or greater than the amount they have '''
-
-
-    return 1            
+    while True:
+        wager = int(input('How many chips do you want to wager? ==> '))
+        if wager <= 0:
+            print('The wager amount must be greater than 0. Please enter again.')
+        elif wager > bank:
+            print('The wager amount cannot be greater than how much you have.', bank)
+        else:
+            return wager
+            
 
 def get_slot_results() -> tuple:
     ''' Returns the result of the slot pull '''
@@ -48,23 +48,37 @@ def get_slot_results() -> tuple:
 
     return 1, 2, 3
 
+
 def get_matches(reela, reelb, reelc) -> int:
     ''' Returns 3 for all 3 match, 2 for 2 alike, and 0 for none alike. '''
 
 
     return 0
 
-def get_bank() -> int:
+# take 1
+def get_bank():
     ''' Returns how many chips the user wants to play with.  Loops until a value greater than 0 and less than 101 '''
+    while True:
+        bank = int(input('How many chips do you want to start with? ==> '))
+        if bank <= 0:
+            print('Too low a value, you can only choose 1 - 100 chips')
+            continue
+        elif bank > 100:
+            print('Too high a value, you can only choose 1 - 100 chips')
+            continue
+        else:
+            return bank
 
 
-    return 0
-
+# take 1
 def get_payout(wager, matches):
     ''' Returns how much the payout is.. 10 times the wager if 3 matched, 3 times the wager if 2 match, and negative wager if 0 match '''
-
-
-    return wager * -1     
+    if matches == 3:
+        return wager*10
+    elif matches == 2:
+        return wager*3
+    else:
+        return -wager     
 
 
 if __name__ == "__main__":
@@ -74,7 +88,7 @@ if __name__ == "__main__":
 
         bank = get_bank()
 
-        while True:  # Replace with condition for if they still have money.
+        while bank > 0:  # Replace with condition for if they still have money.
             
             wager = get_wager(bank)
 
