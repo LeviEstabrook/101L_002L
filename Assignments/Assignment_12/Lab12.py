6d82d97df24ea275ed7d16be00bd4c1d60d754f8
Levi Estabrook <mrlevil2021@gmail.com>
2021-11-29 19:32:15 -0600

    Lab 12 part 1

---
+++ Assignments/Assignment_12/Lab12.py
@@ -0,0 +1,20 @@
+'''
+CS 101 Lab
+Program 12
+Levi Lindskog is my name in Canvas, but I go by Levi Estabrook.
+lhl8r5@umsystem.edu
+
+PROBLEM:
+
+
+ERROR HANDLING:
+
+'''
+
+#ALGORITHM
+
+import turtle
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(50)
