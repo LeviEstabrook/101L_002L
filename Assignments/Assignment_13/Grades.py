62fe52048a6deee0e4ab7c64903703ca9a080de4
Levi Estabrook <mrlevil2021@gmail.com>
2021-12-09 17:09:22 -0600

    Lab 13 part 1

---
+++ Assignments/Assignment_13/Grades.py
@@ -0,0 +1,40 @@
+'''
+CS 101 Lab
+Program 13
+Levi Lindskog is my name in Canvas, but I go by Levi Estabrook.
+lhl8r5@umsystem.edu
+
+PROBLEM:
+Program must be able to be tested on by another program via Unit Testing.
+
+ERROR HANDLING:
+ZeroDivisionError when average function given list with 0 elements,
+fixed by adding a check to the average function for if values list is empty.
+'''
+
+#ALGORITHM
+
+import math
+
+def total(values):
+    total = 0
+    for num in values:
+        total+=num
+    return float(total)
+
+def average(values):
+    if len(values) == 0:
+        return math.nan
+    total = 0
+    for num in values:
+        total+=num
+    return float(total / len(values))
+
+def median(values):
+    values.sort()
+    center = len(values) / 2
+    if center // 1 == center: #even number of elements in list
+        return (values[int(center - 0.5)] + values[int(center + 0.5)]) / 2
+    else: #odd number of elements in list
+        return values[int(center)]
+        
